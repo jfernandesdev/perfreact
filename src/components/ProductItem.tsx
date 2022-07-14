89f056429e1724c02c59a2ea97da25c4dc473b7b
jfernandesdev <jfernandes.dev@gmail.com>
2022-07-14 16:04:21 -0300

    FEAT: memo

---
+++ src/components/ProductItem.tsx
@@ -0,0 +1,19 @@
+import { memo } from 'react';
+
+interface ProductItemProps{
+  product: {
+    id: number;
+    price: number;
+    title: string;
+  }
+}
+
+export function ProductItemComponent({ product }: ProductItemProps ) {
+  return(
+    <div>{product.title} - <strong>R$ {product.price}</strong></div>
+  )
+}
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.product, nextProps.product)
+})
