5e95dad43455e236374291a44339ccc61628017b
jfernandesdev <jfernandes.dev@gmail.com>
2022-07-16 08:58:30 -0300

    REFACTOR: virtualization

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,5 +1,5 @@
-import { useMemo } from 'react';
 import { ProductItem } from "./ProductItem";
+import { List, ListRowRenderer } from 'react-virtualized'
 
 interface SearchResultsProps {
   totalPrice: number;
@@ -13,16 +13,31 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ totalPrice, results, onAddToWishlist }: SearchResultsProps) {
+
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishlist={onAddToWishlist}
+        />
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>Preço total: R$ {totalPrice}</h2>
-      {results.map(product => (
-        <ProductItem 
-          key={product.id} 
-          product={product}
-          onAddToWishlist={onAddToWishlist}
-        />
-      ))}
+
+      <List 
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
+
     </div>
   );
 }
